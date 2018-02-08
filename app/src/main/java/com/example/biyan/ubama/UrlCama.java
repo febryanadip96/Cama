b4cd19eee4470d8d059564df9b15fa4084e25182
febryanadip96 <febryan_dru@yahoo.com>
2018-02-08 18:28:25 +0700

    perbaikan

--- app/src/main/java/com/example/biyan/ubama/UrlCama.java
+++ app/src/main/java/com/example/biyan/ubama/UrlCama.java
@@ -6,7 +6,7 @@ package com.example.biyan.ubama;
 
 public final class UrlCama {
 
-    private final static String IP = "http://192.168.43.243:8000";
+    private final static String IP = "http://192.168.1.110:8000";
 
     private final static String URL_API = IP+"/api/";
 
